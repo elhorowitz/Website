8933b8d24751056197462cb424167cd08b12eef5
elhorowitz <ericalhorowitz@gmail.com>
2014-07-02 15:02:22 -0700

    set up angular for projects

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,1 +1,13 @@
 var app = angular.module('pageDataApp', []);
+
+app.controller('projectListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('projects.json').success(function(data) {
+      $scope.projectList = data;
+      $scope.thisProj = data[0];
+
+      $scope.tabSelect = function(el) {
+        $scope.thisProj = el;
+      };
+    });
+  }]);
