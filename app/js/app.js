d745bb2b466705f7e627ef5fce91af90b660f19e
elhorowitz <ericalhorowitz@gmail.com>
2014-07-08 13:17:37 -0700

    updated images

--- app/js/app.js
+++ app/js/app.js
@@ -1,114 +1,24 @@
-var app = angular.module('pageDataApp', [])
-  .directive('onFinishRender', function ($timeout) {
-    return {
-      restrict: 'A',
-      link: function (scope, element, attr) {
-        if (scope.$last === true) {
-          $timeout(function () {
-              scope.$emit(attr.onFinishRender);
-          });
-        }
-      }
-    };
-});
-
-app.controller('projectListCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('projects.json').success(function(data) {
-      $scope.projectList = data;
-      $scope.thisProj = data[0];
-
-      $scope.tabSelect = function(el) {
-        $scope.thisProj = el;
-      };
-
-      $scope.selected = true;
-
-      $scope.$on('setTinyNav', function(ngRepeatFinishedEvent) {
-
-        //tinynav 
-        $(function () {
-          $("#nav").tinyNav();
-        });
-
-      });
-
-      $scope.$on('setGallery', function(ngRepeatFinishedEvent) {
-        
-        //Magnific-Popup
-        $('.gallery').each(function() { // the containers for all your galleries
-          $(this).magnificPopup({
-            delegate: 'a', // the selector for gallery item
-            type: 'image',
-            gallery: {
-              enabled:true
-            }
-          });
-        });
-      });
-      
-    });
-  }]);
-
-app.controller('contactListCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('contact.json').success(function(data) {
-      $scope.contactList = data;
-    });
-  }]);
-
-
-
 
 ;(function ($) {
   $(function () {
     'use strict';
     
     var self = {},
-      setMediaQuery,
       navSelect
     ;
 
-     //Function to switch between tabs - small screen
-      navSelect = function (el) { 
-        var projectID = el.val();
-        angular.forEach($scope.projectList, function(project) {
-          if (project.id == projectID) {
-            $scope.tabSelect(project);
-            console.log($scope.thisProj);
-          }
-        });
-      };
-
-    //Function for Media Queries
-    setMediaQuery = function (el) {
-      //Only show one image if for mobile
-      if (el) {
-        $(".gallery").append("<p class=\"directions\">Click for more pictures</p>");
-        $(".gallery").each(function() {
-          $(this).find("a:not(:first)").addClass("hidden");
-        });
-      } else {
-        $(".gallery").children("p").remove();
-        $(".thumbnail").removeClass("hidden");   
-      } 
+    //Function to switch between tabs - small screen
+    navSelect = function (el) { 
+      var projectID = el.val();
+      angular.forEach($scope.projectList, function(project) {
+        if (project.id == projectID) {
+          $scope.tabSelect(project);
+          console.log($scope.thisProj);
+        }
+      });
     };
 
-
-
     self.initialize = function () {
-
-      //Switch between tabs - small screen
-      $(".side-nav").on("change", function(e) {
-        e.preventDefault();
-        navSelect($(this).find("select"));
-      });
-
-      //detect media-queries
-      setMediaQuery(Modernizr.mq('(max-width: 41.75em)'));
-      $(window).resize(function() {
-        setMediaQuery(Modernizr.mq('(max-width: 41.75em)'));
-      });
       
       //Smooth Scroll code developed by Chris Coyier and Devin Sturgeon  
       $('a[href*=#]:not([href=#])').click(function() {
--- app/js/controller.js
+++ app/js/controller.js
@@ -1,61 +1,49 @@
-// var app = angular.module('pageDataApp', [])
-//   .directive('onFinishRender', function ($timeout) {
-//     return {
-//       restrict: 'A',
-//       link: function (scope, element, attr) {
-//         if (scope.$last === true) {
-//           $timeout(function () {
-//               scope.$emit(attr.onFinishRender);
-//           });
-//         }
-//       }
-//     };
-// });
+var app = angular.module('pageDataApp', [])
+  .directive('onFinishRender', function ($timeout) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attr) {
+        if (scope.$last === true) {
+          $timeout(function () {
+              scope.$emit(attr.onFinishRender);
+          });
+        }
+      }
+    };
+});
 
-// app.controller('projectListCtrl', ['$scope', '$http',
-//   function ($scope, $http) {
-//     $http.get('projects.json').success(function(data) {
-//       $scope.projectList = data;
-//       $scope.thisProj = data[0];
+app.controller('projectListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('projects.json').success(function(data) {
+      $scope.projectList = data;
+      $scope.thisProj = data[0];
+
+      $scope.tabSelect = function(el) {
+        $scope.thisProj = el;
+      };
+
+      $scope.selected = true;
+
+      $scope.$on('setGallery', function(ngRepeatFinishedEvent) {
+
+        //Magnific-Popup
+        $('.gallery').each(function() { // the containers for all your galleries
+          $(this).magnificPopup({
+            delegate: 'a', // the selector for gallery item
+            type: 'image',
+            gallery: {
+              enabled:true
+            }
+          });
+        });
+      });
       
-//       $scope.$on('setTinyNav', function(ngRepeatFinishedEvent) {
+    });
+  }]);
 
-//         //Switch between tabs - small screen
-//         $(".side-nav").on("change", function(e) {
-//           e.preventDefault();
-//           navSelect($(this).find("select"));
-//         });
-
-//         //tinynav 
-//         $(function () {
-//           $("#nav").tinyNav();
-//         });
-
-//       });
-
-//       $scope.$on('setGallery', function(ngRepeatFinishedEvent) {
-        
-//         //Magnific-Popup
-//         $('.gallery').each(function() { // the containers for all your galleries
-//           $(this).magnificPopup({
-//             delegate: 'a', // the selector for gallery item
-//             type: 'image',
-//             gallery: {
-//               enabled:true
-//             }
-//           });
-//         });
-//       });
-
-//       $scope.tabSelect = function(el) {
-//         $scope.thisProj = el;
-//       };
-//     });
-//   }]);
-
-// app.controller('contactListCtrl', ['$scope', '$http',
-//   function ($scope, $http) {
-//     $http.get('contact.json').success(function(data) {
-//       $scope.contactList = data;
-//     });
-//   }]);
+app.controller('contactListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('contact.json').success(function(data) {
+      $scope.contactList = data;
+    });
+  }]);
